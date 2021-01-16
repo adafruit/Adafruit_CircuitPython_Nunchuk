d39b8d710c5f226c9a3005a34a75aa0409178f61
David Glaude <david.glaude@gmail.com>
2021-01-17 00:19:12 +0100

    Add support for uDraw GameTablet as a module

---
+++ examples/udraw_simpletest.py
@@ -0,0 +1,24 @@
+# SPDX-FileCopyrightText: 2021 David Glaude
+#
+# SPDX-License-Identifier: MIT
+import board
+from adafruit_nunchuk.udraw import uDraw
+
+controller = uDraw(board.I2C())
+
+while True:
+
+    # Pressure: (8-248)
+    if controller.pressure != 8:
+        print("Pen pressure: ", controller.pressure)
+
+    if controller.position != (4095, 4095):
+        print("Pen (x,y): ", controller.position)
+
+    # Buttons: True of False
+    if controller.button_pen:
+        print("Button Pressed: PEN")
+    if controller.button_c:
+        print("Button Pressed: C")
+    if controller.button_z:
+        print("Button Pressed: Z")
