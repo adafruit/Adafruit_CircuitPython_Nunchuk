2a88a27b947781cb2449e59560d485f5c2be6616
Alec Delaney <tekktrik@gmail.com>
2022-02-24 17:04:31 -0500

    Modify class docstring

--- adafruit_nunchuk.py
+++ adafruit_nunchuk.py
@@ -42,17 +42,14 @@ _I2C_INIT_DELAY = 0.1
 
 
 class Nunchuk:
-    """
-    Class which provides interface to Nintendo Nunchuk controller.
+    """Class which provides interface to Nintendo Nunchuk controller.
 
-    :param i2c: The `busio.I2C` object to use.
-    :param address: The I2C address of the device. Default is 0x52.
-    :type address: int, optional
-    :param i2c_read_delay: The time in seconds to pause between the
+    :param ~I2C i2c: The `busio.I2C` object to use.
+    :param int address: (Optional) The I2C address of the device. Default is 0x52.
+    :param float i2c_read_delay: (Optional) The time in seconds to pause between the
         I2C write and read. This needs to be at least 200us. A
         conservative default of 2000us is used since some hosts may
         not be able to achieve such timing.
-    :type i2c_read_delay: float, optional
     """
 
     _Values = namedtuple("Values", ("joystick", "buttons", "acceleration"))
